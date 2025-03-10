a90a2d7809832a864e838303124350ef02492191
r1mke <kerim.begic@edu.fit.ba>
2025-03-10 13:27:19 +0100

    Settings added

--- API/Program.cs
+++ API/Program.cs
@@ -10,7 +10,13 @@ builder.Services.AddDbContext<DataContext>(opt => {
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
+
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins
+("http://localhost:4200",
+"https://localhost:4200"));
 
 app.MapControllers();
 
